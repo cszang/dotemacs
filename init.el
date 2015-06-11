09b14133572cec6a52662c0e1f6c7a0b96e1a702
cszang <zang@eml.cc>
2015-06-11 09:59:18 +0200

    added unfill-paragraph

--- init.el
+++ init.el
@@ -454,6 +454,16 @@
   (newline-and-indent)
   )
 
+;;; Stefan Monnier <foo at acm.org>. It is the opposite of fill-paragraph    
+(defun unfill-paragraph (&optional region)
+  "Takes a multi-line paragraph and makes it into a single line of text."
+  (interactive (progn (barf-if-buffer-read-only) '(t)))
+  (let ((fill-column (point-max)))
+    (fill-paragraph nil region)))
+
+;; Handy key definition
+(define-key global-map "\M-Q" 'unfill-paragraph)
+
 ;; autocomplete tags in Zettelkasten
 (defun cz-complete-zetteltag ()
   (interactive)
