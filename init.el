a2e170a5ab3f946dbadf439bff06ff9e6799e774
cszang <zang@eml.cc>
2015-06-17 09:39:02 +0200

    added magit last-seen-...

--- init.el
+++ init.el
@@ -226,7 +226,10 @@
 
 (use-package magit
   :bind
-  ("C-x g" . magit-status))
+  ("C-x g" . magit-status)
+  :config
+  (setq magit-last-seen-setup-instructions "1.4.0")
+  )
 
 (use-package polymode
   :config
