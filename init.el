428436caebd585afe07882bef514603cfec5e9f8
zang <zang@posteo.de>
2015-06-26 10:25:03 +0200

    no windows anymore, just machine specific settings

--- init.el
+++ init.el
@@ -89,8 +89,11 @@
 (display-time-mode 1)
 (setq display-time-24h-format 1)
 
-(set-face-attribute 'default nil :height 121 :font "Terminus")
-(setq-default line-spacing 3)
+(if (string< "bloom" system-name)
+    (progn (set-face-attribute 'default nil :height 131 :font "Terminus")
+           (setq-default line-spacing 5))
+  (progn (set-face-attribute 'default nil :height 121 :font "Terminus")
+         (setq-default line-spacing 3)))
 
 (load-theme 'zenburn)
 
@@ -410,33 +413,6 @@
   :config
   (add-hook 'markdown-mode-hook 'flyspell-mode)
   (add-hook 'markdown-mode-hook 'auto-fill-mode))
-
-;;;;;;;;;;;;;
-;; Windows ;;
-;;;;;;;;;;;;;
-
-(cond
- ((string-match "mingw" system-configuration)
-  ;; to be _absolutely_ shure, we use unix encoding in all files
-  (set-default buffer-file-coding-system 'utf-8-unix)
-  (set-default-coding-systems 'utf-8-unix)
-  (prefer-coding-system 'utf-8-unix)
-  (set-default default-buffer-file-coding-system 'utf-8-unix)
-  ;; some thing concerning the PATH to mark magit and spell-checking work correctly
-  (setq exec-path (append exec-path '("C:/Program Files (x86)/Git/bin")))
-  (setq magit-git-executable "C:/Program Files (x86)/Git/bin/git.exe")
-  (setq-default ispell-program-name "C:/Program Files (x86)/Aspell/bin/aspell.exe")
-  ;; make sure firefox is found
-  (setq browse-url-firefox-program "C:/Program Files (x86)/Mozilla Firefox/firefox.exe")
-  (setq browse-url-generic-program "C:/Program Files (x86)/Mozilla Firefox/firefox.exe")
-  ;; make org-babel find R on Windows
-  (setq org-babel-R-command "C:/Progra~1/R/R-3.2.0/bin/x64/R.exe --slave --no-save")
-  ;; use bigger font
-  (set-face-attribute 'default nil :height 131 :font "Terminus")
-  ;; add Python installation and scripts to the PATH
-  (setq exec-path (append exec-path '("C:/Python27")))
-  (setq exec-path (append exec-path '("C:/Python27/Scripts")))
-  ))
 
 ;;;;;;;;;;;;;;;;;;;;;;;
 ;; Special functions ;;
