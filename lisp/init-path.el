151b666296ad322fb32b722275b8c61677be91cd
Christian Zang <zang@posteo.de>
2015-10-05 09:55:28 +0200

    updated PATH

--- lisp/init-path.el
+++ lisp/init-path.el
@@ -1,6 +1,7 @@
-(setenv "PATH" (concat (getenv "PATH") ":/usr/local/bin:/usr/texbin"))
+(setenv "PATH" (concat (getenv "PATH") ":/usr/local/bin:/usr/texbin:/Library/TeX/texbin"))
 (setq exec-path (append exec-path '("/usr/texbin")))
 (setq exec-path (append exec-path '("/usr/local/bin")))
+(setq exec-path (append exec-path '("/Library/TeX/texbin")))
 (setq backup-directory-alist `(("." . ,(expand-file-name "~/.emacs.d/backups"))))
 (setq bookmark-default-file "~/ownCloud/Emacs/Lesezeichen")
 (setq diary-file "~/ownCloud/Emacs/Kalender")
