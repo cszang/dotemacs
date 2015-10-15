a1cb43da574d44c279f6d753ac968e38fc765b9f
Christian Zang <zang@posteo.de>
2015-10-15 10:46:55 +0200

    ESS indentation settings

--- lisp/init-ess.el
+++ lisp/init-ess.el
@@ -27,9 +27,7 @@
   (add-hook 'ess-mode-hook 'smartparens-mode)
   (add-hook 'ess-mode-hook 'turn-on-auto-fill)
   (add-hook 'inferior-ess-mode-hook 'turn-on-auto-fill)
-  (defun myindent-ess-hook ()
-    (setq ess-indent-level 2))
-  (add-hook 'ess-mode-hook 'myindent-ess-hook)
+  (setq ess-default-style 'RStudio)
   (define-key ess-mode-map (kbd "C-c C-a") 'cz-insert-R-section)
   (define-key ess-mode-map (kbd "C-c =") 'cz-occur-R-sections))
 
