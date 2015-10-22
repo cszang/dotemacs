f6f8e8943db3404a65d12938eef2082001cc10c7
Christian Zang <zang@posteo.de>
2015-10-22 11:19:02 +0200

    dired tweaks

--- lisp/init-dired.el
+++ lisp/init-dired.el
@@ -1,2 +1,5 @@
 (setq delete-by-moving-to-trash t)
+(require 'dired-x)
+(setq-default dired-omit-files-p t) ; Buffer-local variable
+(setq dired-omit-files (concat dired-omit-files "\\|^\\.DS_Store$\\|^\\.Rhistory"))
 (provide 'init-dired)
