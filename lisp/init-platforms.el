1125bcf317a14846cec70d2dddb15a7770e3479d
Christian Zang <zang@posteo.de>
2015-10-23 09:30:57 +0200

    make sure that Emacs uses homebrew git on OS X

--- lisp/init-platforms.el
+++ lisp/init-platforms.el
@@ -3,6 +3,7 @@
   (setq mac-option-key-is-meta nil)
   (setq mac-command-key-is-meta t)
   (setq mac-command-modifier 'meta)
+  (setq magit-git-executable "/usr/local/bin/git")
   (setq mac-option-modifier nil)))
 
 (cond
