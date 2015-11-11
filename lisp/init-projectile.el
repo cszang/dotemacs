64067a8fab9fca74293a5b2476465d35c4ffd2a0
Christian Zang <zang@posteo.de>
2015-11-11 20:06:21 +0100

    projectile setup

---
+++ lisp/init-projectile.el
@@ -0,0 +1,7 @@
+(projectile-global-mode)
+
+(setq projectile-mode-line (quote
+                            (:eval (format " ☭|%s|"
+                                           (projectile-project-name)))))
+
+(provide 'init-projectile)
