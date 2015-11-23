f7a9f05907eca7eb1e6b071683964017d9908671
Christian Zang <zang@posteo.de>
2015-11-23 13:15:18 +0100

    simpler frame title

--- lisp/init-appearance.el
+++ lisp/init-appearance.el
@@ -39,7 +39,7 @@
 
 (setq global-visual-line-mode t)
 
-(setq frame-title-format '("Servus, Du schaust auf " "%b in Emacs " emacs-version "!"))
+(setq frame-title-format '("%b"))
 (setq smex-prompt-string "I love you. ")
 
 (beacon-mode 1)
