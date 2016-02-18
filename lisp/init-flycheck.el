d968b9240d95832632586fdccb357e830f1bd707
Christian Zang <zang@posteo.de>
2016-02-18 12:26:50 +0100

    fix flycheck setup

--- lisp/init-flycheck.el
+++ lisp/init-flycheck.el
@@ -1,10 +1,12 @@
+(require 'flycheck)
+
 (flycheck-define-checker proselint
   "A linter for prose."
   :command ("proselint" source-inplace)
   :error-patterns
   ((warning line-start (file-name) ":" line ":" column ": "
-        (id (one-or-more (not (any " "))))
-        (message) line-end))
+            (id (one-or-more (not (any " "))))
+            (message) line-end))
   :modes (text-mode markdown-mode gfm-mode))
 
 (add-to-list 'flycheck-checkers 'proselint)
