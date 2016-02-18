f8bb57c30d663df31d39e790c97edd39bfb7b7e6
Christian Zang <zang@posteo.de>
2016-02-18 12:27:58 +0100

    fixed lonesome paren

--- lisp/init-nagging.el
+++ lisp/init-nagging.el
@@ -6,7 +6,6 @@
   (sit-for 3600)
   (with-current-buffer (get-buffer " *Echo Area 0*")
     (setq-local face-remapping-alist '((default (:height 1.0))))))
-  )
 
 (run-with-timer 0 1200 'cz-nag-me)
 
