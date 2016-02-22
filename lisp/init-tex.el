ff9364df777679ad23dee52cbc3e41e0f9d7d8c6
Christian Zang <zang@posteo.de>
2016-02-22 10:06:55 +0100

    removing use-package style init for auctex
    also: clean up of latexmk stuff;  i like Makefiles better, anyway.

--- lisp/init-tex.el
+++ lisp/init-tex.el
@@ -1,47 +1,24 @@
-(use-package reftex
-  :config
-  (setq reftex-cite-format (quote natbib))
-  (setq reftex-cite-prompt-optional-args nil)
-  (setq reftex-cite-cleanup-optional-args t)
-  (setq reftex-enable-partial-scans t)
-  (setq reftex-save-parse-info t)
-  (setq reftex-use-multiple-selection-buffers t)
-  (setq reftex-plug-into-AUCTeX t)
-  (setq reftex-section-levels '(("part" . 0)
-                                ("chapter" . 1)
-                                ("section" . 2)
-                                ("subsection" . 3)
-                                ("subsubsection" . 4)
-                                ("paragraph" . 5)
-                                ("subparagraph" . 6)
-                                ("frametitle" . 7)
-                                ("addchap" . -1)
-                                ("addsec" . -2)
-                                ("begin{frame}" . -3)))
-  (setq reftex-default-bibliography '("~/Library/texmf/bibtex/bib/local/zotero.bib"))
-  )
+(add-hook 'LaTeX-mode-hook 'turn-on-reftex)
+(setq-default TeX-PDF-mode t)
 
-
-(use-package tex-site
-  :ensure auctex
-  :config
-  (add-hook 'LaTeX-mode-hook 'turn-on-reftex)
-  (setq-default TeX-PDF-mode t)
-  (add-hook 'TeX-mode-hook
-            (lambda ()
-              (add-to-list 'TeX-output-view-style
-                           '("^pdf$" "."
-                             "/Applications/Skim.app/Contents/SharedSupport/displayline %n %o %b")))
-            )
-  (setq TeX-view-program-selection '((output-pdf "PDF Viewer")))
-  (setq TeX-view-program-list
-        '(("PDF Viewer" "/Applications/Skim.app/Contents/SharedSupport/displayline -b -g %n %o %b")))
-  (add-hook 'LaTeX-mode-hook (lambda ()
-                               (push
-                                '("latexmk" "latexmk -pdf %s" TeX-run-TeX nil t
-                                  :help "Run latexmk on file")
-                                TeX-command-list)))
-  (add-hook 'TeX-mode-hook '(lambda () (setq TeX-command-default "latexmk")))
-  )
+(setq reftex-cite-format (quote natbib))
+(setq reftex-cite-prompt-optional-args nil)
+(setq reftex-cite-cleanup-optional-args t)
+(setq reftex-enable-partial-scans t)
+(setq reftex-save-parse-info t)
+(setq reftex-use-multiple-selection-buffers t)
+(setq reftex-plug-into-AUCTeX t)
+(setq reftex-section-levels '(("part" . 0)
+                              ("chapter" . 1)
+                              ("section" . 2)
+                              ("subsection" . 3)
+                              ("subsubsection" . 4)
+                              ("paragraph" . 5)
+                              ("subparagraph" . 6)
+                              ("frametitle" . 7)
+                              ("addchap" . -1)
+                              ("addsec" . -2)
+                              ("begin{frame}" . -3)))
+(setq reftex-default-bibliography '("~/Library/texmf/bibtex/bib/local/zotero.bib"))
 
 (provide 'init-tex)
