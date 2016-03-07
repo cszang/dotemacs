898b38c6ea9b4e15c8e30892231591ee5cc1f322
Christian Zang <zang@posteo.de>
2016-03-07 19:38:58 +0100

    no flyspell by default in markdown buffers

--- lisp/init-markdown.el
+++ lisp/init-markdown.el
@@ -1,6 +1,5 @@
 (add-to-list 'auto-mode-alist '("\\.md$" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.mkd$" . markdown-mode))
-(add-hook 'markdown-mode-hook 'flyspell-mode)
 (add-hook 'markdown-mode-hook 'auto-fill-mode)
 
 (provide 'init-markdown)
