d6a9ebca947500b5237d3d6ec8948b7106cf366d
Christian Zang <zang@posteo.de>
2016-06-08 12:44:31 +0200

    fill/unfill paragraph toggle

--- lisp/init-convenience.el
+++ lisp/init-convenience.el
@@ -1,12 +1,17 @@
-;;; Stefan Monnier <foo at acm.org>. It is the opposite of fill-paragraph    
-(defun unfill-paragraph (&optional region)
-  "Takes a multi-line paragraph and makes it into a single line of text."
-  (interactive (progn (barf-if-buffer-read-only) '(t)))
-  (let ((fill-column (point-max)))
-    (fill-paragraph nil region)))
+;;; fill/unfill paragraph as toggle with M-q, via
+;;; http://endlessparentheses.com/fill-and-unfill-paragraphs-with-a-single-key.html
+(defun endless/fill-or-unfill ()
+  "Like `fill-paragraph', but unfill if used twice."
+  (interactive)
+  (let ((fill-column
+         (if (eq last-command 'endless/fill-or-unfill)
+             (progn (setq this-command nil)
+                    (point-max))
+           fill-column)))
+    (call-interactively #'fill-paragraph)))
 
-;; Handy key definition
-(define-key global-map "\M-Q" 'unfill-paragraph)
+(global-set-key [remap fill-paragraph]
+                #'endless/fill-or-unfill)
 
 ;; occur TODOs
 (defun cz-occur-TODOs ()
