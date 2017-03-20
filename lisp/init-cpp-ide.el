36c3c94fab336466cc7661eacb5205f5d58acef5
Christian Zang <christian.zang@wzw.tum.de>
2017-03-20 10:41:43 +0100

    better C++ support!

--- lisp/init-cpp-ide.el
+++ lisp/init-cpp-ide.el
@@ -1,34 +1,9 @@
-(add-hook 'c-mode-hook #'ggtags-mode)
-(add-hook 'c++-mode-hook #'ggtags-mode)
-(setq-default c-basic-offset 2)
-(setq c-default-style "linux")
-(add-to-list 'auto-mode-alist '("\\.h\\'" . c++-mode))
-(setq xah-lookup-browser-function 'eww)
-(defun xah-lookup-cppreference (&optional word)
-  "Lookup definition of current word or text selection in URL."
-  (interactive)
-  (xah-lookup-word-on-internet
-   word
-   "http://en.cppreference.com/mwiki/index.php?search=�"
-   xah-lookup-browser-function))
-(define-key c++-mode-map (kbd "C-c d") #'xah-lookup-cppreference)
-
-(define-key ggtags-mode-map (kbd "C-c g s") 'ggtags-find-other-symbol)
-(define-key ggtags-mode-map (kbd "C-c g h") 'ggtags-view-tag-history)
-(define-key ggtags-mode-map (kbd "C-c g r") 'ggtags-find-reference)
-(define-key ggtags-mode-map (kbd "C-c g f") 'ggtags-find-file)
-(define-key ggtags-mode-map (kbd "C-c g c") 'ggtags-create-tags)
-(define-key ggtags-mode-map (kbd "C-c g u") 'ggtags-update-tags)
-
-(define-key ggtags-mode-map (kbd "M-,") 'pop-tag-mark)
-
 (setq speedbar-show-unknown-files t)
 
-(add-hook 'after-init-hook 'global-company-mode)
-(delete 'company-semantic company-backends)
-(define-key c-mode-map  [(tab)] 'company-complete)
-(define-key c++-mode-map  [(tab)] 'company-complete)
-(add-to-list 'company-backends 'company-c-headers)
+;; (add-hook 'after-init-hook 'global-company-mode)
+;; (define-key c-mode-map  [(tab)] 'company-complete)
+;; (define-key c++-mode-map  [(tab)] 'company-complete)
+;; (add-to-list 'company-backends 'company-c-headers)
 
 ;; setup GDB
 (setq
@@ -38,3 +13,28 @@
  ;; Non-nil means display source file containing the main routine at startup
  gdb-show-main t
  )
+
+(add-to-list 'load-path "/usr/local/share/emacs/site-lisp/rtags/")
+
+(require 'rtags) ;; optional, must have rtags installed
+(cmake-ide-setup)
+(setq cmake-ide-flags-c++
+      '("/Library/Developer/CommandLineTools/usr/bin/../include/c++/v1"
+        "/usr/local/include"
+        "/Library/Developer/CommandLineTools/usr/bin/../lib/clang/8.0.0/include"
+        "/Library/Developer/CommandLineTools/usr/include"
+        "/usr/include"))
+
+(global-flycheck-mode 1)
+
+(require 'rtags)
+(require 'company-rtags)
+
+(setq rtags-completions-enabled t)
+(eval-after-load 'company
+  '(add-to-list
+    'company-backends 'company-rtags))
+(setq rtags-autostart-diagnostics t)
+(rtags-enable-standard-keybindings)
+
+(provide 'init-cpp-ide)
