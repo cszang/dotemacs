beec0159f737a23d558519a94d7c3b66e107db1e
Christian Zang <christian.zang@wzw.tum.de>
2017-03-23 12:04:47 +0100

    excellent font!

--- lisp/init-appearance.el
+++ lisp/init-appearance.el
@@ -18,7 +18,7 @@
 (if (string= "buck" system-name)
     (progn (set-face-attribute 'default nil :height 131 :font "Inconsolata")
            (setq-default line-spacing 3))
-  (progn (set-face-attribute 'default nil :height 130 :font "Monoid")
+  (progn (set-face-attribute 'default nil :height 151 :font "Input")
          (setq-default line-spacing 5)))
 
 (load-theme 'zenburn)
