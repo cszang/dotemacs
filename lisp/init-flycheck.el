47eb6b3d2cea01a90684944ae0c41d7fba366e59
Christian Zang <christian.zang@wzw.tum.de>
2017-07-13 19:52:39 +0200

    flycheck updates

--- lisp/init-flycheck.el
+++ lisp/init-flycheck.el
@@ -6,10 +6,13 @@
   :error-patterns
   ((warning line-start (file-name) ":" line ":" column ": "
             (id (one-or-more (not (any " "))))
-            (message) line-end))
+            (message (one-or-more not-newline)
+                     (zero-or-more "\n" (any " ") (one-or-more not-newline)))
+            line-end))
   :modes (text-mode markdown-mode gfm-mode))
 
 (add-to-list 'flycheck-checkers 'proselint)
+
 (setq flycheck-global-modes '(not org-mode))
 (global-flycheck-mode 1)
 
