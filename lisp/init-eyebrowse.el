c7f5d8b38deffdca0c7715afd26b2bf26fb42a28
Christian Zang <christian.zang@wzw.tum.de>
2017-09-05 09:18:49 +0200

    save desktop on exit

--- lisp/init-eyebrowse.el
+++ lisp/init-eyebrowse.el
@@ -1,2 +1,7 @@
 (eyebrowse-mode t)
+;; use desktop-mode for restoring session; one desktop is enough
+(desktop-save-mode 1)
+(setq desktop-path '("~/.emacs.d/"))
+(setq desktop-dirname "~/.emacs.d/")
+(setq desktop-base-file-name "emacs-desktop")
 (provide 'init-eyebrowse)
