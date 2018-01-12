a1029c963a018d9b6882bc69b2850cfeadc5b511
Christian Zang <christian.zang@wzw.tum.de>
2018-01-12 15:05:49 +0100

    larger default face

--- lisp/init-appearance.el
+++ lisp/init-appearance.el
@@ -18,7 +18,7 @@
 (if (string= "buck" (system-name))
     (progn (set-face-attribute 'default nil :height 131 :font "Inconsolata")
            (setq-default line-spacing 3))
-  (progn (set-face-attribute 'default nil :height 131 :font "Input")
+  (progn (set-face-attribute 'default nil :height 141 :font "Input")
          (setq-default line-spacing 5)))
 
 (load-theme 'zenburn)
